a1e54536d49b74249591ebf462172429ce945ebb
Spencer Smith <spencer.smith2012@gmail.com>
2018-04-11 11:04:42 -0600

    Updated model creation so that it works properly

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ var isPlaying = false;
 var setSettingsString = 'Set Settings';
 var pulsed = false;
 var closeNow = false;
-var model;
-var databaseModelName = 'model1';
+var model = new NeuralNetLearner();
+var databaseModelName = 'model2';
 var numGamesPerBatch = 10;
 var db = firebase.firestore();
 var numGamesPlayed=0;
 
-model = new NeuralNetLearner();
 createOrLoadModel();
 
 window.onload = function () {
@@ -25,10 +24,13 @@ window.onload = function () {
 
 function createOrLoadModel() {
     db.collection(databaseModelName).orderBy('timestamp', 'desc').limit(1).get().then((querySnapshot) => {
-      if (querySnapshot.empty)
-        // parameters: input, output, hidden layers, activation function, learning rate
+      if (querySnapshot.empty) {
+        console.log("Creating new model...");
+        // parameters: in, out, layers, activation, learning rate
         model.createModel(65, 1, [65, 65], 'sigmoid', 0.3);
+      }
       else {
+        console.log("Loading model...");
         model.loadModelFromJsonString(querySnapshot.docs[0].data().model);
       }
     });
@@ -58,6 +60,8 @@ function gameDone() {
   } else {
     currentGameNum = 0;
     var data = getDataFromDB();
+    console.log("Data from db for training: ");
+    console.log(data);
     window.lastTimeSaved = Date.now();
     var features = data.map((d) => d.slice(0,-1));
     var labels = data.map((d) => d[65]);
@@ -80,15 +84,19 @@ function getDataFromDB() {
   var data = [];
 
   if (typeof window.lastTimeSaved !== 'undefined'){
-    db.collection(window.databaseName).where('timestamp', '>', window.lastTimeSaved).get().then((querySnapshot) => {
+    db.collection(window.databaseName).where('timestamp', '>', window.lastTimeSaved).limit(100).get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         data.push(doc.data().state);
+      }).catch(err => {
+        console.log(err);
       });
     });
   } else {
-    db.collection(window.databaseName).get().then((querySnapshot) => {
+    db.collection(window.databaseName).limit(100).get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         data.push(doc.data().state);
+      }).catch(err => {
+        console.log(err);
       });
     });
   }
@@ -101,7 +109,6 @@ function saveModelToDB() {
   prev = document.getElementById("output0").innerHTML;
   document.getElementById("output0").innerHTML = prev + "<br />Saving model to database... ";
 
-  // console.log(model);
   db.collection(databaseModelName).add({
       model: model.getModelAsJsonString(),
       timestamp: Date.now()
